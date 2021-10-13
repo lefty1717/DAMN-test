6265a13cb4513e1f06810813ec5b2330d6bb65a6
Cuboid <yyytaiwanzed@gmail.com>
2021-10-14 00:08:13 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./scss/all.css";
+import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
+import AddIcon from "@material-ui/icons/Add";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Damn</h1>
+      <AccessAlarmIcon />
+      <AddIcon />
     </div>
   );
 }

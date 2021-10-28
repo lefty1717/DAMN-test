d9910a4437604b9b71f4dabee52623b3471be8b8
Cuboid <yyytaiwanzed@gmail.com>
2021-10-28 17:26:08 +0800

    bottom  navbar finish

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from "react";
 import "./scss/all.css";
 import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
 import AddIcon from "@material-ui/icons/Add";
+import BottomNav from "./components/BottomNav";
 function App() {
   return (
     <div className="app">
       <h1>Damn</h1>
       <AccessAlarmIcon />
       <AddIcon />
+      <BottomNav />
       {/* React Context API */}
       {/* React Router */}
       {/* AI 小當家 */}
-      {/* firebase 資料庫建立 */}
+      {/* firebase 資料庫建立 f */}
       {/* bottom nav */}
     </div>
   );
--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import AccountCircleIcon from "@material-ui/icons//AccountCircle";
+import KitchenIcon from "@material-ui/icons//Kitchen";
+import MenuBookIcon from "@material-ui/icons//MenuBook";
+import { AppBar, IconButton, Toolbar } from "@material-ui/core";
+import { Box } from "@mui/system";
 
 const BottomNav = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="bottomNav">
+      <AppBar className="" position="fixed" color="white">
+        <Toolbar>
+          <IconButton
+            className="bottomNav__button active"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ display: "flex", flexDirection: "column" }}
+          >
+            <MenuBookIcon />
+            <p className="fs-1 m-unset">食譜</p>
+          </IconButton>
+          {/* <StyledFab color="secondary" aria-label="add">
+              <KitchenIcon />
+            </StyledFab> */}
+          {/* <Box sx={{ flexGrow: 1 }} /> */}
+          <IconButton color="inherit" className="bottomNav__button">
+            <KitchenIcon />
+            <p className="fs-1">冰箱管理</p>
+          </IconButton>
+          <IconButton color="inherit" className="bottomNav__button">
+            <AccountCircleIcon />
+            <p className="fs-1"> 個人</p>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 
-export default BottomNav
+export default BottomNav;

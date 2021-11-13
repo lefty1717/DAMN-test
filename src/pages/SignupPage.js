e653ef4f971171c7e250b332d0fc2662988f6757
lefty1717 <hank1717@gmail.com>
2021-11-13 13:09:49 +0800

    loginPage and signPage

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
 
 const SignupPage = () => {
     return (
-        <div>
-            Sign up
+        <div className="login-signup-Page">
+            <Grid className="box">
+                <Card className="card1">
+                <div>
+                    <input type="text" placeholder="姓名"></input><br/>
+                    <input type="text" placeholder="Email"></input><br/>
+                    <input type="text" placeholder="密碼"></input><br/>
+                    <input type="text" placeholder="確認密碼"></input>
+                </div>
+                </Card>
+
+                <Card className="cardC2">
+                <Button fullWidth>註冊</Button>
+                </Card>
+            </Grid>
         </div>
     )
 }

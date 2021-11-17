b697467150d37c3bb5940b44107e6db435ac5368
Cuboid <yyytaiwanzed@gmail.com>
2021-11-18 00:31:22 +0800

    bottom navbar insert AI

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -3,35 +3,64 @@ import AccountCircleIcon from "@material-ui/icons//AccountCircle";
 import KitchenIcon from "@material-ui/icons//Kitchen";
 import MenuBookIcon from "@material-ui/icons//MenuBook";
 import { AppBar, IconButton, Toolbar } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+const btnList = [
+  {
+    id: 1,
+    title: "食譜",
+    icon: <MenuBookIcon />,
+    routeTo: "/",
+  },
+  {
+    id: 2,
+    title: "冰箱管理",
+    icon: <KitchenIcon />,
+    routeTo: "/fridge",
+  },
+  {
+    id: 3,
+    title: "個人",
+    icon: <AccountCircleIcon />,
+    routeTo: "/profile",
+  },
+];
 
 const BottomNav = () => {
-  const btnList = [
-    {
-      id: 1,
-      title: "食譜",
-      icon: <MenuBookIcon />,
-      routeTo: "/",
-    },
-    {
-      id: 2,
-      title: "冰箱管理",
-      icon: <KitchenIcon />,
-      routeTo: "/fridge",
-    },
-    {
-      id: 3,
-      title: "個人",
-      icon: <AccountCircleIcon />,
-      routeTo: "/profile",
-    },
-  ];
   const [activeBtnId, setActiveBtnId] = useState(1);
-
+  let history = useHistory();
   const handleActiveClass = (id) => {
-    // console.log(id);
     setActiveBtnId(id);
   };
+  const commands = [
+    {
+      command: ["開啟冰箱", "打開冰箱", "冰箱頁面"],
+      callback: () => {
+        setActiveBtnId(2);
+        history.push("/fridge");
+      },
+      isFuzzyMatch: true, // 模糊匹配
+      fuzzyMatchingThreshold: 0.9, // 高於 80% 才確定
+      bestMatchOnly: true,
+    },
+    {
+      command: ["開啟個人頁面", "打開個人頁面", "個人頁面"],
+      callback: () => {
+        setActiveBtnId(3);
+        history.push("/profile");
+      },
+    },
+    {
+      command: ["回到首頁", "打開首頁", "開啟首頁", "首頁"],
+      callback: () => {
+        setActiveBtnId(1);
+        history.push("/");
+      },
+    },
+  ];
+  useSpeechRecognition({ commands });
   return (
     <div className="bottomNav">
       <AppBar>

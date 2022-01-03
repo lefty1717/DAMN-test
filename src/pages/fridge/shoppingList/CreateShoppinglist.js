4ba8d003bbde1de5b30430432a2a5ef9a48d5584
lefty1717 <hank1717@gmail.com>
2022-01-03 11:10:21 +0800

    新增購物清單(修復版)

--- src/pages/fridge/shoppingList/CreateShoppinglist.js
+++ src/pages/fridge/shoppingList/CreateShoppinglist.js
@@ -8,12 +8,22 @@ import { db, storage } from '../../../firebase';
 import { collection, addDoc } from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
+import { Button, Input } from '@mui/material';
+import { borderRadius, Box, ThemeProvider, width } from "@mui/system";
+import IconButton from "@mui/material/IconButton";
+import CustomIcon from "../../../components/Icon";
+import { styled } from "@mui/material/styles";
+
 
 function CreateShoppinglist () {
+
+    const ThumbnailInput = styled("input")({
+        display: "none",
+    });
     //購物清單結構
     const [shoppingList, setShoppingList] = useState({
         name:"",
-        quantity:0,
+        quantity:"",
         unit:"",
         notes:"",
         startDate:0,
@@ -27,57 +37,119 @@ function CreateShoppinglist () {
     const handleClick = function(e){
         setShoppingList({...shoppingList,[e.target.name]:e.target.value})
     }
+    
+    const handleThumbnail = (e) => {
+        const thumbnail = {
+          file: e.target.files[0],
+          url: URL.createObjectURL(e.target.files[0]),
+        };
+        setShoppingList({...shoppingList,imageURL:thumbnail.url, imageFile:thumbnail.file})
+    };
+    console.log(shoppingList.imageFile);
 
     //uplode image
-    const upload = async function(e){
-        const imageRef = ref(storage, e.target.files[0].name);
-        await uploadBytes(imageRef, e.target.files[0]);
+    const upload = async function(file){
+        const imageRef = ref(storage,`food/${file.name}`);
+        await uploadBytes(imageRef, file);
         const url = await getDownloadURL(imageRef);
-        setShoppingList({...shoppingList, imageURL: url})
+        return url
     }
 
     //add to firebase
     async function addData(){
+        const imgurl = await upload(shoppingList.imageFile)
+        
+        console.log(imgurl);
+
         const docRef = await addDoc(collection(db, 'users', '3HuEsCE9jUlCm68eBQf4', 'shoppingList'),{
-        name:shoppingList.name,
-        quantity:shoppingList.quantity,
-        unit:shoppingList.unit,
-        notes:shoppingList.notes,
-        startDate:shoppingList.startDate,
-        endDate:shoppingList.endDate,
-        isFrozen:shoppingList.isFrozen,
-        ingredientTags:shoppingList.ingredientTags,
-        imageURL:shoppingList.imageURL,
+            name:shoppingList.name,
+            quantity:shoppingList.quantity,
+            unit:shoppingList.unit,
+            notes:shoppingList.notes,
+            startDate:shoppingList.startDate,
+            endDate:shoppingList.endDate,
+            isFrozen:shoppingList.isFrozen,
+            ingredientTags:shoppingList.ingredientTags,
+            imageURL:imgurl,
         });
     }
 
     return(
         <div className="CreateShoppinglist">
             <TopBar />
-            <UploadButtons  onChange={upload} />
+            <label htmlFor="icon-button-file">
+            <Box
+                component="div"
+                sx={{
+                display: "flex",
+                flexDirection:"column",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "white",
+                width: "309px",
+                height: "240px",
+                borderRadius: "20px",
+                margin: " 0 0 0 33px",
+                }}
+            >
+                <img src={shoppingList?.imageURL} alt="" loading="lazy" />
+                <ThumbnailInput
+                accept="image/*"
+                id="icon-button-file"
+                type="file"
+                name='imageURL'
+                onChange={handleThumbnail}
+                />
+                <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="span"
+                style={{
+                    display: `${shoppingList.imageURL ? "none" : "unset"}`,
+                }}
+                >
+                <CustomIcon
+                    size={80}
+                    name="AddPhotoAlternateIcon"
+                    hidden={shoppingList.imageURL ? true : false}
+                    color='#C7E3EE'
+                />
+                </IconButton>
+            </Box>
+            </label>
             <div className="input-container"> 
                 <div className="input-foodname">
                     <h5>食物名稱：</h5>
-                    <FreeSolo />
+                    <Input type="text" name="name" value={shoppingList.name} onChange={handleClick}/><br/>
                 </div>
 
                 <div className="input-amount">
                     <h5>數 量：</h5>
-                    <FreeSolo />
+                    <Input type="number" name="quantity" value={shoppingList.quantity} onChange={handleClick}/>
                 </div>
 
                 <div className="input-unit">
                     <h5>單 位：</h5>
-                    <FreeSolo />
+                    <Input type="text" name="unit" value={shoppingList.unit} onChange={handleClick}/>
                 </div>
 
                 <div className="input-remark">
                     <h5>備 註：</h5>
-                    <FreeSolo />
+                    <Input type="text" name="notes" value={shoppingList.notes} onChange={handleClick}/>
                 </div>
 
                 <div className="submit">
-                    <BasicButtons/>
+                    <Button 
+                    sx={{
+                        backgroundColor: "white",
+                        color:"#C7E3EE",
+                        borderRadius: "10px",
+                    }}
+                    size="large"
+                    onClick={addData}
+                    >
+                        送出
+                    </Button>
                 </div>
             </div>
         </div>
